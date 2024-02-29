5fb3f140cb44eeedc31348bebf3dbdbfe586dffe
nikhilmanoj <60520523+nikhilmanoj@users.noreply.github.com>
2024-03-01 00:38:53 +0530

    Update spoti.py

--- spoti.py
+++ spoti.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="YOUR_APP_CLIENT_ID",
-                                                           client_secret="YOUR_APP_CLIENT_SECRET"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="8a3bf93750f54ed4b1aea814928df5eb",
+                                                           client_secret="91b1672f2bd74b598e676396365c0224"))
 
 results = sp.search(q='weezer', limit=20)
 for idx, track in enumerate(results['tracks']['items']):
