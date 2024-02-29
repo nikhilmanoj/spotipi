6be3744dc0eafa94c5c3d5eb7c8e832f0fe8aaf5
nikhilmanoj <60520523+nikhilmanoj@users.noreply.github.com>
2024-03-01 01:07:08 +0530

    Update track.py

--- track.py
+++ track.py
@@ -1,10 +1,12 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
-# Set up Spotify client credentials
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+# Set up Spotify OAuth
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id="8a3bf93750f54ed4b1aea814928df5eb",
-    client_secret="91b1672f2bd74b598e676396365c0224"
+    client_secret="91b1672f2bd74b598e676396365c0224",
+    redirect_uri="http://localhost:8888/callback",
+    scope="user-read-playback-state"
 ))
 
 # Retrieve information about the currently playing track
